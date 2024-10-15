09d140b7f77ecc6ad6729ed345c72fd5179c3bd7
Mario Perez <mario.perez415@gmail.com>
2024-10-15 14:27:10 -0400

    Setup dynamic rendering

--- src/components/LandingPage/index.tsx
+++ src/components/LandingPage/index.tsx
@@ -1,7 +1,35 @@
+import { hero, imageText, dataSection } from "@/data";
+import DataSection from "./DataSection";
+import Hero from "./Hero";
+import ImageText from "./ImageText";
+import { SectionType, Section as TSection } from "@/entities";
+
+const data = [hero, imageText, dataSection, dataSection, dataSection];
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const SECTION: Record<SectionType, any> = {
+    hero: Hero,
+    "image-text": ImageText,
+    data: DataSection,
+};
+
+type SectionProps = TSection;
+const Section = ({ type, ...props }: SectionProps) => {
+    if (!type) {
+        return null;
+    }
+
+    const Component = SECTION[type];
+
+    return <Component {...props} />;
+};
+
 export default function LandingPage() {
     return (
         <div className="prose size-full">
-            <h1>Landing Page</h1>
+            {data.map((section, idx) => (
+                <Section key={idx} {...section} />
+            ))}
         </div>
     );
 }
