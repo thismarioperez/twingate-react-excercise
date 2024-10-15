adc68d6441c42b33c707fe6946622c85c02a790f
Mario Perez <mario.perez415@gmail.com>
2024-10-15 15:18:02 -0400

    Tweak hero size

--- src/components/LandingPage/Hero.tsx
+++ src/components/LandingPage/Hero.tsx
@@ -11,7 +11,7 @@ export default function Hero({ imageURI }: Props) {
     }
 
     return (
-        <div className="h-36 w-full py-4">
+        <div className="h-60 w-full py-4">
             <img className="m-0 size-full object-cover" src={imageURI} />
         </div>
     );
